84281b9118500b40cc9e6b7e6a4ee37ed4794a1c
cprkv <mnz-rrh@ya.ru>
2022-12-07 04:43:32 +0500

    initial commit

---
+++ Arguments.cs
@@ -0,0 +1,61 @@
+﻿using MatthiWare.CommandLine;
+
+namespace merge_subs;
+
+public class Arguments
+{
+  public string MkvPath { get; set; }
+  public string SubPath { get; set; }
+  public string SubPattern { get; set; }
+  public string MkvToolnix { get; set; }
+
+  public static Arguments Parse( string[] args )
+  {
+    var parser = new CommandLineParser<Arguments>();
+
+    parser.Configure( o => o.MkvPath )
+      .Name( "m", "mkv" )
+      .Description( "path to mkv files" )
+      .Required();
+
+    parser.Configure( o => o.SubPath )
+      .Name( "s", "sub" )
+      .Description( "path to subtitle files" )
+      .Required();
+
+    parser.Configure( o => o.SubPattern )
+      .Name( "e", "ext" )
+      .Description( "subtitle files pattern" )
+      .Default( "*.srt" )
+      .Required( false );
+
+    parser.Configure( o => o.MkvToolnix )
+      .Name( "t", "mkvToolnix" )
+      .Description( "mkv toolnix installation directory" )
+      .Default( "C:\\Program Files\\MKVToolNix" )
+      .Required( false );
+
+    var parserResult = parser.Parse( args );
+
+    if ( parserResult.HasErrors )
+      throw new Exception( string.Join( Environment.NewLine, parserResult.Errors.Select( x => x.Message ) ) );
+
+    var arguments = parserResult.Result;
+
+    Console.WriteLine( $"MkvPath:    {arguments.MkvPath}" );
+    Console.WriteLine( $"SubPath:    {arguments.SubPath}" );
+    Console.WriteLine( $"SubExt:     {arguments.SubPattern}" );
+    Console.WriteLine( $"MkvToolnix: {arguments.MkvToolnix}" );
+
+    if ( !Directory.Exists( arguments.MkvPath ) )
+      throw new Exception( $"mkv path points to invalid directory: {arguments.MkvPath}" );
+
+    if ( !Directory.Exists( arguments.SubPath ) )
+      throw new Exception( $"sub path points to invalid directory: {arguments.SubPath}" );
+
+    if ( !Directory.Exists( arguments.MkvToolnix ) )
+      throw new Exception( $"mkv toolnix points to invalid directory: {arguments.MkvToolnix}" );
+
+    return parserResult.Result;
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿
+using merge_subs;
+using System.Diagnostics;
+
+var arguments = Arguments.Parse( args );
+var mkvFiles = Directory.GetFiles( arguments.MkvPath, "*.mkv" );
+var subFiles = Directory.GetFiles( arguments.SubPath, $"{arguments.SubPattern}" );
+
+if ( mkvFiles.Length != subFiles.Length )
+  Console.Error.WriteLine( $"mkv files count ({mkvFiles.Length}) not match sub files count ({subFiles.Length})" );
+
+var mergedDir = Path.Join( arguments.MkvPath, "merged" );
+if ( !Directory.Exists( mergedDir ) )
+  Directory.CreateDirectory( mergedDir );
+
+foreach ( var subPath in subFiles )
+{
+  var name = Path.GetFileNameWithoutExtension( Path.GetFileName( subPath ) );
+  //Console.WriteLine( name );
+
+  var mkvPath = Path.Join( arguments.MkvPath, Path.ChangeExtension( name, ".mkv" ) );
+
+  if ( !File.Exists( mkvPath ) )
+  {
+    Console.Error.WriteLine( $"corresponding mkv file for '{subPath}' not found. searching was: '{mkvPath}'" );
+    continue;
+  }
+
+  var mergedPath = Path.Join( mergedDir, Path.ChangeExtension( name, ".mkv" ) );
+
+  if ( File.Exists( mergedPath ) )
+  {
+    Console.WriteLine( $"WARNING: {mergedPath} already exists. skipping" );
+    continue;
+  }
+
+  {
+    var commonPrefix = CommonPrefix( mkvPath, subPath, mergedPath );
+    Console.WriteLine();
+    Console.WriteLine(
+      $"'{mkvPath.Remove( 0, commonPrefix.Length )}' \n" +
+      $"'{subPath.Remove( 0, commonPrefix.Length )}' \n" +
+      $"    => '{mergedPath.Remove( 0, commonPrefix.Length )}'" );
+  }
+
+  using var process = new Process
+  {
+    StartInfo = new ProcessStartInfo
+    {
+      FileName = Path.Join( arguments.MkvToolnix, "mkvmerge.exe" ),
+      ArgumentList =
+      {
+        "-o",
+        mergedPath,
+        mkvPath,
+        subPath,
+      },
+      UseShellExecute = false,
+      RedirectStandardOutput = true,
+      CreateNoWindow = true,
+    }
+  };
+
+  process.Start();
+  process.WaitForExit();
+
+  if ( process.ExitCode != 0 )
+  {
+    Console.Error.WriteLine( $"process exited with bad exit code" );
+  }
+}
+
+static string CommonPrefix( params string[] strings )
+{
+  var commonPrefix = strings.FirstOrDefault() ?? "";
+
+  foreach ( var s in strings )
+  {
+    var potentialMatchLength = Math.Min( s.Length, commonPrefix.Length );
+
+    if ( potentialMatchLength < commonPrefix.Length )
+      commonPrefix = commonPrefix.Substring( 0, potentialMatchLength );
+
+    for ( var i = 0; i < potentialMatchLength; i++ )
+    {
+      if ( s[ i ] != commonPrefix[ i ] )
+      {
+        commonPrefix = commonPrefix.Substring( 0, i );
+        break;
+      }
+    }
+  }
+
+  return commonPrefix;
+}
